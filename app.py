5dd939bf8027798bbae9ae4f4658d91aba6490e2
mridulgulati18 <mridulgulati18@gmail.com>
2024-04-11 15:18:38 +0530

    Caching

--- app.py
+++ app.py
@@ -173,7 +173,7 @@ def fetch_data_from_google_sheets(_secrets):
                 df = pd.DataFrame(data[1:], columns=data[0])
                 all_data[cmp_symbol] = df
                 cnt += 1
-                if cnt % 23 == 0:
+                if cnt % 10 == 0:
                     time.sleep(105)
             return all_data
         
